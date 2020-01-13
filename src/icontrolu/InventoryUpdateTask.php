ebd2d19f79a26103bed4a67cca61ebba2eb5b0ba
Noa Heyl <me@falkirks.com>
2020-01-13 07:36:56 -0800

    potential fix for #10
    From PR #11. Thanks to @artulloss

--- src/icontrolu/InventoryUpdateTask.php
+++ src/icontrolu/InventoryUpdateTask.php
@@ -4,9 +4,18 @@ namespace icontrolu;
 use pocketmine\scheduler\Task;
 
 class InventoryUpdateTask extends Task{
+    /** @var iControlU $owner */
+    private $owner;
+    /**
+     * InventoryUpdateTask constructor.
+     * @param iControlU $owner
+     */
+    public function __construct(iControlU $owner) {
+        $this->owner = $owner;
+    }
     public function onRun(int $tick) : void{
         /** @var iControlU $owner */
-        $owner = $this->getOwner();
+        $owner = $this->owner;
         foreach($owner->s as $session){
             $session->syncInventory();
         }
